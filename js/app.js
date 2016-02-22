e9fb2047066912fe7f2a0f201ba0731c43ecae19
sduplantis18 <sduplantis18@gmail.com>
2016-02-21 20:32:42 -0500

    Many updates

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    });
+    $('.ryu').mouseenter(function() {
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+  });
+    $('.ryu').mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    });
+    $('.ryu').mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+  {'left': '1020px'},
+  500,
+  function() {
+    $(this).hide();
+    $(this).css('left', '520px');
+  }
+);
+    });
+    $('.ryu').mouseup(function() {
+        $('.ryu-ready').show();
+        $('.ryu-throwing').hide();
+    });
+    
+    $(document).keydown(function(e){
+        if (e.keyCode == 88) {
+            $('.ryu-still').hide();
+            $('.ryu-cool').show();
+            }
+        }).keyup(function (e) {
+        if (e.keyCode == 88) {
+                $('.ryu-cool').hide();
+                $('.ryu-still').show();
+            }
+        });
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
